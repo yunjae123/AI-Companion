3b85ecb731df33dbf5470393f733871321357c3c
Yun Chang <yunjae0500@yahoo.com>
2023-09-09 00:34:47 -0600

    Rename venv folder

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from langchain import OpenAI, LLMChain, PromptTemplate
-from langchain.memory import ConversationBufferWindowMemory
-from dotenv import find_dotenv, find_dotenv
-import requests
-from playsound import playsound
-import os
+from langchain import OpenAI, LLMChain, PromptTemplate #Allows us to run openAI, large language models,
+from langchain.memory import ConversationBufferWindowMemory #Allows us to get the chat history
+from dotenv import find_dotenv, find_dotenv #Allows us to import the .env file
+import requests #https requests
+from playsound import playsound #Allows us to play the audio file
+import os #More env compatibility
 
+load_dotenv(find_dotenv)
