a58a89903c14ca84f253c1293223cbbef5a2da8f
Yun Chang <yunjae0500@yahoo.com>
2023-09-09 00:26:21 -0600

    Add initial packages and update requirements.txt

---
+++ app.py
@@ -0,0 +1,7 @@
+from langchain import OpenAI, LLMChain, PromptTemplate
+from langchain.memory import ConversationBufferWindowMemory
+from dotenv import find_dotenv, find_dotenv
+import requests
+from playsound import playsound
+import os
+
