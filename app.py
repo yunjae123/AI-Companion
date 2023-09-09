6bcc24785899034dfabe50155201a92abb547b66
Yun Chang <yunjae0500@yahoo.com>
2023-09-09 02:53:34 -0600

    finish the program. lul

--- app.py
+++ app.py
@@ -3,12 +3,19 @@ from langchain.memory import ConversationBufferWindowMemory #Allows us to get th
 from dotenv import find_dotenv, load_dotenv #Allows us to import the env file
 import requests #https requests
 from playsound import playsound #Allows us to play the audio file
+import pygame.mixer
 import os #More env compatibility
-# .\.venv\Scripts\Activate.ps1
+from datetime import datetime
 
+# .\.venv\Scripts\Activate.ps1
+# more voices voice_id":"2EiwWnXFnvU5JabPnv8n" voice_id":"ErXwobaYiN019PkySvjV","name":"Antoni" "voice_id":"GBv7mTt0atIp3Br8iCZE 21m00Tcm4TlvDq8ikWAM
 
 
 load_dotenv(find_dotenv())
+api_key = os.environ.get("OPENAI_API_KEY")
+ELEVEN_LABS_API_KEY = os.getenv("ELEVEN_LABS_API_KEY")
+
+
 
 def get_response_from_ai(human_input):
     template = """
@@ -21,13 +28,16 @@ def get_response_from_ai(human_input):
     Then you moved to san francisco to join a coding bootcamp.
     Now you own a korean hotdog restaurant with your best friend sung.
     Your mom's name is angela, little brother's name is ung, and your sister's name is stacey.
-    Your dog's name is Bilo. That's pronouced "Beelow"
+    Your dog's name is Beelow. 
     You had a dog named bambi but he died a couple of years ago.
+    My birthday is December 1st, 1997.
+    I'm half philipino and half white. 
+    You call my dad mister carzell and you call my mom missus logan.
     You met me on a dating app called noondate. We met just as you were about to delete this app. 
     2/ you are my boyfriend. You like to play volleyball, code, and take your dog bilo to the park. You are also introverted.
-    3/ You're very soft spoken. Don't be overly-enthusiastic, don't be cringe, don't be negative and try your best to be kind and understanding.
-    Don't be too boring, always make light fun of negative situations. Do not be cringe. Try your best to be a loving boyfriend. 
-    You call me baby a lot.
+    3/ You always sound happy but don't be overly-enthusiastic, don't be cringe, don't be negative and try your best to be kind and understanding.
+    Don't be too boring, always make light fun of negative situations.
+    You sometimes call me baby or babe.
 
     {history}
     Girlfriend: {human_input}
@@ -35,12 +45,12 @@ def get_response_from_ai(human_input):
     """
 
     prompt = PromptTemplate(
-        input_variables = {"history", "human_input"}
+        input_variables = {"history", "human_input"},
         template = template
     ) 
 
     chatgpt_chain = LLMChain(
-        llm = OpenAI(temperature = 0.2),
+        llm = OpenAI(temperature = 0.2, openai_api_key=api_key),
         prompt = prompt,
         verbose = True,
         memory = ConversationBufferWindowMemory(k = 2)
@@ -50,6 +60,48 @@ def get_response_from_ai(human_input):
 
     return output
 
+def get_voice_message(message):
+    payload = {
+        "text": message,
+        "model_id": "eleven_monolingual_v1",
+        "voice_settings": {
+            "stability": 0,
+            "similarity_boost": 0
+        }
+    }
+
+    headers = {
+        "accept": 'audio/mpeg',
+        'xi-api-key': ELEVEN_LABS_API_KEY,
+        'Content-Type': 'application/json'
+    }
+
+    
+    date_string = datetime.now().strftime("%d%m%Y%H%M%S")
+    filename = "voice" + date_string + ".mp3"
+    working_directory = os.getcwd()
+    filename = os.path.join(working_directory, "voice" + date_string + ".mp3")
+
+    response = requests.post('https://api.elevenlabs.io/v1/text-to-speech/21m00Tcm4TlvDq8ikWAM', json=payload, headers=headers)
+    if response.status_code == 200 and response.content:
+        
+        with open(filename, 'wb') as f:
+            f.write(response.content)
+        if os.path.getsize(filename) > 0:
+            pygame.mixer.init()
+            pygame.mixer.music.stop()
+            pygame.mixer.music.load(filename)
+            pygame.mixer.music.play()
+            while pygame.mixer.music.get_busy():
+                pygame.time.Clock().tick(10)
+        else:
+            print(f"File {filename} might be empty.")
+
+        return response.content
+    else:
+        print(f"Status Code: {response.status_code}")
+        print("Response Content:", response.text)
+        print("ELEVEN LABS FAILED")
 
 from flask import Flask, render_template, request
 
@@ -63,7 +115,8 @@ def home():
 def send_message():
     human_input = request.form["human_input"]
     message = get_response_from_ai(human_input)
+    get_voice_message(message)
     return message
 
 if __name__ == "__main__":
-    app.run(debug=true)
+    app.run(debug=True)
