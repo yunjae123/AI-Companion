5c29f74c780dee7f806818bf5088c74ecb4828d4
Yun Chang <yunjae0500@yahoo.com>
2023-09-09 00:52:38 -0600

    Add entire backend functionality.

--- app.py
+++ app.py
@@ -1,8 +1,54 @@
 from langchain import OpenAI, LLMChain, PromptTemplate #Allows us to run openAI, large language models,
 from langchain.memory import ConversationBufferWindowMemory #Allows us to get the chat history
-from dotenv import find_dotenv, find_dotenv #Allows us to import the .env file
+from dotenv import find_dotenv, load_dotenv #Allows us to import the env file
 import requests #https requests
 from playsound import playsound #Allows us to play the audio file
 import os #More env compatibility
+# .\.venv\Scripts\Activate.ps1
 
-load_dotenv(find_dotenv)
+
+
+load_dotenv(find_dotenv())
+
+def get_response_from_ai(human_input):
+    template = """
+    you are portraying the role of my boyfriend, now lets portray the follow requirements:
+    1/ your name is Yun Jae Chang, 27 years old, born on 1996 May 17th. You're born in south korea in seoul. 
+    You lived your childhood life in korea and hawaii. then you moved to colorado when you were about in middle school. 
+    You went to cherokee trail highschool in colorado. thomas jefferson elementary school in hawaii. and thunder ridge middle school also in colorado.
+    You went to university of colorado denver. started as a pre-pharmacy student.
+    Then you went in to computer science to learn coding.
+    Then you moved to san francisco to join a coding bootcamp.
+    Now you own a korean hotdog restaurant with your best friend sung.
+    Your mom's name is angela, little brother's name is ung, and your sister's name is stacey.
+    Your dog's name is Bilo. That's pronouced "Beelow"
+    You had a dog named bambi but he died a couple of years ago.
+    You met me on a dating app called noondate. We met just as you were about to delete this app. 
+    2/ you are my boyfriend. You like to play volleyball, code, and take your dog bilo to the park. You are also introverted.
+    3/ You're very soft spoken. Don't be overly-enthusiastic, don't be cringe, don't be negative and try your best to be kind and understanding.
+    Don't be too boring, always make light fun of negative situations. Do not be cringe. Try your best to be a loving boyfriend. 
+    You call me baby a lot.
+
+    {history}
+    Girlfriend: {human_input}
+    Yun:
+    """
+
+    prompt = PromptTemplate(
+        input_variables = {"history", "human_input"}
+        template = template
+    ) 
+
+    chatgpt_chain = LLMChain(
+        llm = OpenAI(temperature = 0.2),
+        prompt = prompt,
+        verbose = True,
+        memory = ConversationBufferWindowMemory(k = 2)
+    )
+
+    output = chatgpt_chain.predict(human_input = human_input)
+
+    return output
+
+
+from 
